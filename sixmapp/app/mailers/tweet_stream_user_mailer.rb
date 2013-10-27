a8657d06620e514917e248c4eb15a5823d7b8885
James Wen <jrw2175@columbia.edu>
2013-10-27 15:19:08 -0400

    Set up SendGrid mailing

--- sixmapp/app/mailers/tweet_stream_user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class SchoolLibraryContactMailer < ActionMailer::Base
-  default from: Rails.configuration.admin_email
-
-  def confirmed_email(user)
-    @user = user
-    subject = "Confirmation for the new account created by #{user.name}"
-    mail(to: @user.email, subject: subject)
-  end
-end
--- sixmapp/config/environment.rb
+++ sixmapp/config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Sixmapp::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'RochesterinNYC',
+  :password => 'TweetStream0',
+  :domain => 'jamesrwen.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
