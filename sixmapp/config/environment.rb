399563a0fae65f488ce3cb2764d8959654e16d80
James Wen <jrw2175@columbia.edu>
2013-10-29 11:27:36 -0400

    Mailer working

--- sixmapp/config/environment.rb
+++ sixmapp/config/environment.rb
@@ -2,14 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-Sixmapp::Application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'RochesterinNYC',
-  :password => 'TweetStream0',
-  :domain => 'jamesrwen.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+Sixmapp::Application.initialize! 
