a68d3853e57c6829109a673a4a927dd3f27af420
Jeffrey Scholz <jss2245@columbia.edu>
2013-10-30 05:58:21 -0400

    add puma for concurrency, fix search bugs

---
+++ sixmapp/lib/daemons/tweet.rb
@@ -0,0 +1,9 @@
+
+require File.dirname(__FILE__) + "/../../config/environment"
+
+$running = true;
+
+while($running) do
+  sleep 1
+  puts "hello"
+end
--- sixmapp/lib/stream_api.rb
+++ sixmapp/lib/stream_api.rb
@@ -15,13 +15,68 @@ class TweetStreamAPI
     config.oauth_token        = '1944743442-3pV23TQz68mT0GeyoINA6HDwRoe78UvWwLRLWuY'
     config.oauth_token_secret = 'w6hMGXVwNKCjL60jhEDXJs1nzhMP0r5Rmsxoa7wSRc'
   #  config.auth_method        = :oauth
+  end 
+=begin
+TweetStream::Daemon.new('tracker').track('term1', 'term2') do |status|
+  puts status.text
+end
+=end
+
+  # getting tweets must be run on a separate thread
+  # or the everything else will get blocked.
+  # Because tweet_array can be accessed by the tweet_get_thread
+  # and by get_tweets, we must surround it with mutex.synchronize
+  # so that only one thread can use the array at a time. We don't
+  # want a simultaneous read and write or we might get funny values
+
+=begin
+  @@initialized = false
+  @@stop_thread = true
+  def start_tweet_get_thread term0, term1, nterm0, nterm1
+    puts term0; puts term1
+    tsc = TweetStream::Client.new
+    tweet_get_thread = Thread.new do
+      if(@@stop_thread and @@initialized)
+        puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+        puts "^^^^ you tried to stop me ^^^^^^"
+        puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+        @@stop_thread = false
+        tsc.stop if @@initialized
+        @@initialized = true
+        puts "!!!!!!!! Initialized is true !!!!!!!!!!"
+      end
+      tsc.track(term0,term1) do |status, client|
+        
+        if(@@stop_thread)
+          tsc.stop
+          puts "this never prints"
+          puts "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+          puts "^^^^ you tried to stop me ^^^^^^"
+          puts "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+          @@stop_thread = false
+        end
+        puts term0; puts term1; puts nterm0; puts nterm1
+        puts status.text
+        tweet = status.text.downcase
+        rejected = false
+        #rejected = true if (nterm0 != nil and tweet.include? nterm0.downcase)         
+        #rejected = true if (nterm1 != nil and tweet.include? nterm1.downcase)         
+        unless rejected
+          puts status.text
+          @@mutex.synchronize {@@tweet_array.push status.text}
+        end 
+      end
+    end
   end
+=end
 
   @@term0 = ""
   @@term1 = ""
   @@first_wakeup = true
   @@stop = false
   @@reset_terms = false
+  
+  def start_th
   @@stream_th = Thread.new do
     while true 
       puts "!!!!!!!!!!!!! SLEEPING !!!!!!!!!!!!!!"
@@ -54,6 +109,7 @@ class TweetStreamAPI
       puts "##### BROKE EEEEE"
     end
   end
+  end
   
 
   # note that the thread is started and stopped every time 
@@ -66,26 +122,27 @@ class TweetStreamAPI
   @@nterm1_old = ""
   @@nothing = Array.new
   def get_tweets term0, term1, nterm0, nterm1
+    start_th if @@first_wakeup
 
     puts "term0 == #{term0 == nil}"; puts "term1 == #{term1 ==nil}"
     # case 1: blank terms
     if( (term0 == nil and term1 == nil) or (term0 == "" and term1 == "") )
       puts "!!!!!!! nothing !!!!!!!"
       unless @@first_wakeup
-        @@stop = true
+        @@stream_th.terminate
+        while(@@stream_th.alive?)
+          puts "die! die! die!"
+        end   
       end
       return @@nothing
-    
-
     elsif ( @@first_wakeup )
-      @@first_wakeup = false
+      
+      @@stream_th.wakeup
       @@term0_old = term0
       @@term1_old = term1
       @@term0 = term0
       @@term1 = term1
 
-      @@stream_th.wakeup
-      sleep 1
       @@mutex.synchronize {return @@tweet_array}
 
 
@@ -97,12 +154,14 @@ class TweetStreamAPI
       @@term0 = term0
       @@term1 = term1
 
+      @@stream_th.terminate
+      while(@@stream_th.alive?)
+        puts "die! die! die!"
+      end
+          
+
+      start_th
       
-      @@reset_terms = true
-      sleep 1
-      @@stream_th.wakeup unless @@stream_th.alive?
-      
-      sleep 1
       @@mutex.synchronize {return @@tweet_array}
     
     # case 3: same terms as last time
@@ -118,4 +177,24 @@ class TweetStreamAPI
   end
 
 end
- 
+=begin
+      if( @@term0_tmp == term0 and @@term1_tmp == term1 and @@nterm0_tmp == nterm0 and @@nterm1_tmp == nterm1)
+          puts "**** SAME ****"
+          puts "!!!!!!!!!!!!!!"
+          @@mutex.synchronize {return @@tweet_array}
+      else
+          puts "**** DIFFERENT ****"
+          puts "$$$$$$$$$$$$$$$$$$$"
+          @@stop_thread = true
+          @@term0_tmp = term0
+          @@term1_tmp = term1
+          @@nterm0_tmp = nterm0
+          @@nterm1_tmp = nterm1
+          start_tweet_get_thread term0, term1, nterm0, nterm1
+          @@mutex.synchronize {return @@tweet_array}
+      end
+    
+  end 
+=end
+
+
