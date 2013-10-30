75082ccbcf859de480b8cfe79b2cdea2eada8d9c
Jeffrey Scholz <jss2245@columbia.edu>
2013-10-30 06:01:01 -0400

    un-uncomment auth code

--- sixmapp/app/controllers/application_controller.rb
+++ sixmapp/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-#  before_filter :require_user
+  before_filter :require_user
 
   def current_user
     #@current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -10,11 +10,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   
   def require_user
-#    if current_user
-#      return true
-#    end
-#    redirect_to root_url
-#        flash[:notice] = "Please login or sign up."
+    if current_user
+      return true
+    end
+    redirect_to root_url
+        flash[:notice] = "Please login or sign up."
   end
 
   def render_success_plain(body)
