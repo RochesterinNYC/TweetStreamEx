d97c632ec7fd49a4ec96b6166608bc9052009f2f
Jeffrey Scholz <jeffreyscholz@Jeffreys-MacBook-Pro.local>
2013-11-30 00:10:46 -0500

    finished security checks... for now

--- sixmapp/app/controllers/stream_controller.rb
+++ sixmapp/app/controllers/stream_controller.rb
@@ -7,9 +7,23 @@ class StreamController < ApplicationController
   #nil parameters result if the parameters weren't
   #in the URL. Otherwise, it is a zero length string
   def index
+    @warning
+
+    #people can hardwire as many bogus params in the
+    #URL as they want. If there are a zillion of them
+    #we will have a denial of service iterating over
+    #and array of 5 million....    
+    #we shouldn't allow more than we need we need to 
+    #check this first before we start doing loops on 
+    #the array
+    if params.size > 11
+      puts "SOMETHING IS UP"
+      @warning = "please click search and try again"
+    end
 
     defaults = {:keywords => "", :exclude => "", :language=> "en", :latitude=> "", :longitude=> "", :radius=> "", :distance=> ""}
     params.replace(defaults.merge(params))
+
     #for debugging
     params.each do |key,value|
       puts "Param #{key}: #{value} #{value.length}"
@@ -17,7 +31,6 @@ class StreamController < ApplicationController
 
     #is it just me or should this sanitizing code go somewhere else?
     # - Jeffrey Scholz
-    @warning
     if params[:keywords].length > 255
       @warning = "input too long!"
     end
@@ -34,7 +47,8 @@ class StreamController < ApplicationController
     #checking decimal numbers in a range a regex is a pain, so we'll
     #let twitter deal with that. We won't check boundary conditions
     #but we will make sure they only enter a digit with something
-    #reasonable before and after the decimal place
+    #reasonable before and after the decimal place. This is a security
+    #feature, not a gps validator
     unless /(^[-]?([\d]?[\d]?)(\.|$|\.$)[\d]{0,20}$|^$)/.match params[:latitude]
       @warning = "hmm... that latitude won't work"
     end
@@ -42,6 +56,16 @@ class StreamController < ApplicationController
     unless /(^[-]?[\d]?[\d]?[\d]?(\.|$|\.$)[\d]{0,20}$|^$)/.match params[:longitude]
       @warning = "hmm... that longitude won't work"
     end
+
+    #same thing with the distance, security feature, not useability
+    #check. let twitter do the tough legwork
+    unless /(^[\d]{0,20}[.]?{0,20}$|^$)/.match params[:radius]
+      @warning = "that's not a valid radius"
+    end
+
+    #unless /(^[\d]{0,20}[.]?{0,20}$|^$)/.match params[:distance]
+    #  @warning = "that's not a valid distance"
+    #end
 
     unless @warning
       @tweets = @@stream.get_tweets params[:keywords], params[:exclude],
